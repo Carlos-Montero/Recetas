2e45ee1c87bbc8c56173b8d417d464136ae2dedd
Carlos <carlosmonteroreina@gmail.com>
2019-01-14 17:03:44 +0100

    Tercer commit.

--- app/src/main/java/com/example/carlos/recetas/SecondActivity.java
+++ app/src/main/java/com/example/carlos/recetas/SecondActivity.java
@@ -1,4 +1,42 @@
 package com.example.carlos.recetas;
 
-public class SecondActivity {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<Paso>PasoList=new ArrayList<>();
+
+//************************************************************************
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_item_activity_2);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+        //PREPARE DATA
+        preparePasoData();
+
+
+    }
+
 }
+
